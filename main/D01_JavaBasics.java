e69f54f5584513acef6d514a6080fbea756c3a4b
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2024-03-14 09:21:38 +0100

    Update Demo 1

--- main/D01_JavaBasics.java
+++ main/D01_JavaBasics.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Random;
+
 public class D01_JavaBasics {
 
   public static void arithmeticOperations() {
@@ -18,6 +20,11 @@ public class D01_JavaBasics {
     c += 1;
     c++;
 
+    double result7 = Math.sqrt(49);
+
+    Random myRandom = new Random();
+    int randomNumber = myRandom.nextInt(1, 101);
+
     System.out.println("result1: " + result1);
     System.out.println("result2: " + result2);
     System.out.println("result3: " + result3);
@@ -26,6 +33,83 @@ public class D01_JavaBasics {
     System.out.println("result6: " + result6);
     System.out.println("d1: " + d1);
     System.out.println("i1: " + i1);
+    System.out.println("result7: " + result7);
+    System.out.println("randomNumber: " + randomNumber);
+  }
+
+  public static void cases() {
+    /* Bedingte Anweisungen */
+    int age = 17;
+    char gender = 'w';
+
+    // Logische Operatoren: &&, ||, !
+    // Vergleichsoperatoren: >, <, >=, <=, !=, ==
+    // Bedingungs-Operator: ?
+    if (gender == 'm' && age >= 12 && age < 18) {
+      System.out.println("männlicher Teenager");
+    } else if (gender == 'w') {
+      System.out.println("weiblich");
+    } else {
+      System.out.println("divers, männliches Kind, Mann");
+    }
+
+    String genderText;
+    if (gender == 'm') {
+      genderText = "männlich";
+    } else if (gender == 'w') {
+      genderText = "weiblich";
+    } else {
+      genderText = "divers";
+    }
+    System.out.println(genderText);
+
+    genderText = (gender == 'm') ? "männlich" : (gender == 'w') ? "weiblich" : "divers";
+    System.out.println(genderText);
+
+    /* Fallunterscheidungen */
+    switch (gender) {
+      case 'M':
+      case 'm':
+        genderText = "männlich";
+        break;
+      case 'W':
+      case 'w':
+        genderText = "weiblich";
+        break;
+      default:
+        genderText = "divers";
+        break;
+    }
+    System.out.println(genderText);
+
+    genderText = switch (gender) {
+      case 'M', 'm' -> "männlich";
+      case 'W', 'w' -> "weiblich";
+      default -> "divers";
+    };
+    System.out.println(genderText);
+
+  }
+
+  public static void loops() {
+    int i = 0;
+    while (i < 10) {
+      System.out.println(i + 1);
+      i++;
+    }
+
+    i = 0;
+    do {
+      System.out.println(i + 1);
+      i++;
+    } while (i < 10);
+
+    for (i = 0; i < 1000; i++) {
+      if (i % 2 == 0) {
+        continue; // break;
+      }
+      System.out.println(i + 1);
+    }
   }
 
   public static void main(String[] args) {
@@ -36,6 +120,14 @@ public class D01_JavaBasics {
 
     /* Arithmetische Operatoren und Typumwandlungen */
     arithmeticOperations();
+    System.out.println();
+
+    /* Bedingte Anweisungen und Fallunterscheidungen */
+    cases();
+    System.out.println();
+
+    /* Schleifen */
+    loops();
     System.out.println();
 
   }
